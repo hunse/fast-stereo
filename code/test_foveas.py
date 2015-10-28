fae11b94c68f2d34b959a38e98987b21d65e7892
Eric Hunsberger <erichuns@gmail.com>
2015-10-28 16:56:48 -0400

    Multiple foveas

--- code/test_foveas.py
+++ code/test_foveas.py
@@ -36,11 +36,16 @@ fine_disp = coarse_bp(frame, values=values, down_factor=0, iters=iters, **params
 fine_disp *= 2**frame_down_factor
 toc()
 
-fovea_corner = (60, 360)
-fovea_shape = (80, 80)
+# fovea_corners = (60, 360)
+# fovea_shapes = (80, 80)
+fovea_corners = [(60, 160), (60, 360)]
+fovea_shapes = [(80, 80), (80, 80)]
+
+fovea_corners = np.array(fovea_corners, ndmin=2)
+fovea_shapes = np.array(fovea_shapes, ndmin=2)
 
 tic()
-foveal_disp = foveal_bp(frame, fovea_corner, fovea_shape, values=values, iters=iters, **params)
+foveal_disp = foveal_bp(frame, fovea_corners, fovea_shapes, values=values, iters=iters, **params)
 foveal_disp *= 2**frame_down_factor
 toc()
 
@@ -62,7 +67,8 @@ disp_args = dict(interpolation='none', vmin=0, vmax=full_values)
 axes[2].imshow(coarse_disp, **disp_args)
 axes[4].imshow(fine_disp, **disp_args)
 axes[6].imshow(foveal_disp, **disp_args)
-plot_fovea(fovea_corner, fovea_shape, ax=axes[6])
+for fovea_corner, fovea_shape in zip(fovea_corners, fovea_shapes):
+    plot_fovea(fovea_corner, fovea_shape, ax=axes[6])
 
 coarse_up = cv2.pyrUp(coarse_disp)[:fine_disp.shape[0], :fine_disp.shape[1]]
 img = axes[3].imshow(coarse_up.astype(float) - fine_disp, interpolation='none')
